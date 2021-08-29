c97236475a2f72890fad50331073570e83e3d7e0
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-28 23:57:15 -0600

    Created activity form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
 interface Props {
@@ -18,6 +19,7 @@ export default function ActivityDashboard({ activities }: Props) {
                 {activities[0] &&
                     <ActivityDetails activity={activities[0]} />
                 }
+                <ActivityForm />
             </Grid.Column>
         </Grid>
     )
