a93410319feb1d4b57b94d19139f65eb3a8c393b
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-29 11:29:24 -0600

    Handled create and edit submission

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -12,10 +12,11 @@ interface Props {
     cancelSelectActivity: () => void;
     editMode: boolean;
     openForm: (id: string) => void;
-    closeForm: () => void
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -33,7 +34,7 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                     />
                 }
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
                 }
             </Grid.Column>
         </Grid>
