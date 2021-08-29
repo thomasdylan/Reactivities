a746f55bd481f6b51c470c0eb3ea4c035cef8de2
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-29 11:03:51 -0600

    Added display to create and edit forms

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -10,9 +10,12 @@ interface Props {
     selectedActivity: Activity | undefined;
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -22,10 +25,16 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                 />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity &&
-                    <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} />
+                {selectedActivity && !editMode &&
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    />
                 }
-                <ActivityForm />
+                {editMode &&
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />
+                }
             </Grid.Column>
         </Grid>
     )
