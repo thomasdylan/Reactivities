2c2e514e343d116fd96f68822cf9615c442a928b
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-29 18:02:58 -0600

    Connected form submit to database

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { request } from 'http';
 import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
@@ -29,7 +30,11 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities')
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => axios.post<void>('/activities', activity),
+    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
 const agent = {
