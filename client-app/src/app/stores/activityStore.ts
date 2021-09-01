fc5a83719703072514b9098e68be6e0e2987bf94
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-31 18:32:30 -0600

    Added action for MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,35 @@
 import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
 
 export default class ActivityStore {
-    title = 'MobX';
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';
+    loadActivites = async () => {
+        this.setLoadingInitial(true);
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+            })
+            this.setLoadingInitial(false);
+
+        } catch (error) {
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
     }
 }
